a9c2452e3c4c4a4dc6141017ae91c6413d61aced
Alix17 <wildflowerinabox@gmail.com>
2021-11-23 14:52:58 -0800

    changing type since its reserved

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -3,19 +3,24 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
 });
 
-const workoutSchema = new mongoose.Schema({
-  day: Date,
-  exercises: [
-    {
-      type: String,
-      name: String,
-      duration: Number,
-      weight: Number,
-      reps: Number,
-      sets: Number,
-    },
-  ],
-});
+const workoutSchema = new mongoose.Schema(
+  {
+    day: Date,
+    exercises: [
+      {
+        type: String,
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+      },
+    ],
+  },
+  {
+    typeKey: "$type",
+  }
+);
 
 const workoutModel = mongoose.model("Workout", workoutSchema);
 
